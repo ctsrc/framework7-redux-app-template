a2d818723ce7ef23e6795f07731fc07f5a17d45a
Erik Nordstrøm <erik@nordstroem.no>
2019-02-04 17:35:47 +0100

    Static build of cfc565c6085b4fb340dba3b80ea4e94eb346eab9.

---
+++ precache-manifest.3443220f5b732c7c7dcb29499a9ae286.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e12e34a54d8d15914719",
+    "url": "/framework7-redux-app-template/static/css/main.3110e4a9.chunk.css"
+  },
+  {
+    "revision": "e12e34a54d8d15914719",
+    "url": "/framework7-redux-app-template/static/js/main.e12e34a5.chunk.js"
+  },
+  {
+    "revision": "f8fb9660ee530a1296e9",
+    "url": "/framework7-redux-app-template/static/css/1.8edb2702.chunk.css"
+  },
+  {
+    "revision": "f8fb9660ee530a1296e9",
+    "url": "/framework7-redux-app-template/static/js/1.f8fb9660.chunk.js"
+  },
+  {
+    "revision": "5c23a196c445818f4c27",
+    "url": "/framework7-redux-app-template/static/js/runtime~main.5c23a196.js"
+  },
+  {
+    "revision": "40303237e5669d23ff19f503286afb97",
+    "url": "/framework7-redux-app-template/static/media/Framework7Icons-Regular.40303237.woff2"
+  },
+  {
+    "revision": "18bfd1d8f77ae61222e32b657e866695",
+    "url": "/framework7-redux-app-template/static/media/Framework7Icons-Regular.18bfd1d8.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/framework7-redux-app-template/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/framework7-redux-app-template/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "c2b585dac46dfe951ebbb322abad07bd",
+    "url": "/framework7-redux-app-template/static/media/Framework7Icons-Regular.c2b585da.eot"
+  },
+  {
+    "revision": "696fe062b45f43381ed5d108cb4d41d6",
+    "url": "/framework7-redux-app-template/static/media/Framework7Icons-Regular.696fe062.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/framework7-redux-app-template/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/framework7-redux-app-template/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "fc86dbd72bb66947fcd153ae29009f5f",
+    "url": "/framework7-redux-app-template/index.html"
+  }
+];
